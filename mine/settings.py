fe34a5d76611dc40ce12c4a67c8a880cccf70201
Nikhil <97503662+nikhilhvr@users.noreply.github.com>
2022-02-24 17:42:30 +0530

    main

--- mine/settings.py
+++ mine/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from tkinter import TRUE
+
+from numpy import True_
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wdy^&+(t@_a668m*8u*yrzzg025!%18^2u&#%5(no=rii1p_le'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','mlseven7.herokuapp.com','appmlseven.herokuapp.com']
 
