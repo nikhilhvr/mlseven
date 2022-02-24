4a5a2b7cb65ad783506690a937f8ecc9333a6e75
Nikhil <97503662+nikhilhvr@users.noreply.github.com>
2022-02-24 18:20:06 +0530

    main

--- home/views.py
+++ home/views.py
@@ -1,4 +1,22 @@
+from distutils.command import config
 from django.shortcuts import render, HttpResponse
+import pyrebase
+config={
+'apiKey': "AIzaSyCzjggEgn1MFChFIgyQ-moe7LcOgOkfoPY",
+  'authDomain': "ml-7-bf914.firebaseapp.com",
+  'projectId': "ml-7-bf914",
+  'storageBucket': "ml-7-bf914.appspot.com",
+  'messagingSenderId': "442104626326",
+  'appId': "1:442104626326:web:58be149e1b9c55054696b7",
+  'measurementId': "G-K8EVRB01ZV"
+    
+}
+firebase =pyrebase.intializeApp(config);
+
+auth= firebase.auth()
+
+
+
 
 def home (request):
     return render(request, 'index.html')
